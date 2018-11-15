42f6a274b4c09157509d36795772e3541c036731
Vinicius Vianna <vinicius-durap-16@hotmail.com>
2018-11-15 14:29:57 -0200

    alteração da classe ProdutoDAO; criação da classe ProdutoService

--- src/main/java/br/com/sudosu/sortemotores/dao/ProdutoDAO.java
+++ src/main/java/br/com/sudosu/sortemotores/dao/ProdutoDAO.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +13,9 @@ import java.util.List;
  *
  * @author Vinicius Vianna
  */
-public class ProdutoDAO extends BaseDAO{
-    public Produto getProdutoById(Long id) throws SQLException{
+public class ProdutoDAO extends BaseDAO {
+
+    public Produto getProdutoById(Long id) throws SQLException {
         Connection conn = null;
         PreparedStatement stmt = null;
         try {
@@ -26,7 +28,7 @@ public class ProdutoDAO extends BaseDAO{
                 rs.close();
                 return p;
             }
-        }finally{
+        } finally {
             if (stmt != null) {
                 stmt.close();
             }
@@ -36,22 +38,23 @@ public class ProdutoDAO extends BaseDAO{
         }
         return null;
     }
-    public List<Produto>findByName(String name) throws SQLException{
+
+    public List<Produto> findByName(String name) throws SQLException {
         List<Produto> produtos = new ArrayList<>();
         Connection conn = null;
         PreparedStatement stmt = null;
         try {
             conn = getConnection();
             stmt = conn.prepareStatement("select * from produto where lower(nome) like ?");
-            stmt.setString(1, "%" + name.toLowerCase() +"%");
+            stmt.setString(1, "%" + name.toLowerCase() + "%");
             ResultSet rs = stmt.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 Produto p = createProduto(rs);
                 produtos.add(p);
             }
             rs.close();
-        } finally{
-              if (stmt != null) {
+        } finally {
+            if (stmt != null) {
                 stmt.close();
             }
             if (conn != null) {
@@ -60,22 +63,23 @@ public class ProdutoDAO extends BaseDAO{
         }
         return produtos;
     }
-    public List<Produto>findByModelo(String modelo) throws SQLException{
+
+    public List<Produto> findByModelo(String modelo) throws SQLException {
         List<Produto> produtos = new ArrayList<>();
         Connection conn = null;
         PreparedStatement stmt = null;
         try {
             conn = getConnection();
             stmt = conn.prepareStatement("select * from produto where lower(modelo) like ?");
-            stmt.setString(1, "%" + modelo.toLowerCase() +"%");
+            stmt.setString(1, "%" + modelo.toLowerCase() + "%");
             ResultSet rs = stmt.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 Produto p = createProduto(rs);
                 produtos.add(p);
             }
             rs.close();
-        } finally{
-              if (stmt != null) {
+        } finally {
+            if (stmt != null) {
                 stmt.close();
             }
             if (conn != null) {
@@ -84,7 +88,8 @@ public class ProdutoDAO extends BaseDAO{
         }
         return produtos;
     }
-    public List<Produto>getProduto() throws SQLException{
+
+    public List<Produto> getProduto() throws SQLException {
         List<Produto> produtos = new ArrayList<>();
         Connection conn = null;
         PreparedStatement stmt = null;
@@ -92,13 +97,13 @@ public class ProdutoDAO extends BaseDAO{
             conn = getConnection();
             stmt = conn.prepareStatement("select * from produto");
             ResultSet rs = stmt.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 Produto p = createProduto(rs);
                 produtos.add(p);
             }
             rs.close();
-        } finally{
-              if (stmt != null) {
+        } finally {
+            if (stmt != null) {
                 stmt.close();
             }
             if (conn != null) {
@@ -107,7 +112,8 @@ public class ProdutoDAO extends BaseDAO{
         }
         return produtos;
     }
-    public Produto createProduto(ResultSet rs) throws SQLException{
+
+    public Produto createProduto(ResultSet rs) throws SQLException {
         Produto p = new Produto();
         p.setIdProduto(rs.getLong("idProduto"));
         p.setNome(rs.getString("nome"));
@@ -115,8 +121,77 @@ public class ProdutoDAO extends BaseDAO{
         p.setValorCompra(rs.getDouble("valorCompra"));
         p.setValorVenda(rs.getDouble("valorVenda"));
         p.setQtde(rs.getInt("quantidade"));
-        
-        
+        return p;
+    }
+
+    public void save(Produto p) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            conn = getConnection();
+            if (p.getIdProduto() == null) {
+                stmt = conn.prepareStatement("insert into produto(nome, marca, modelo, codigo, valorCompra, valorVenda, quantidade)values('?, ?, ?, ?, ?, ?, ?)",
+                        Statement.RETURN_GENERATED_KEYS);
+            } else {
+                stmt = conn.prepareStatement("update produto set nome=?,marca=?, modelo=? , codigo=?, valorCompra=?, valorVenda=?, quantidade=?");
+            }
+            stmt.setString(1, p.getNome());
+            stmt.setString(2, p.getMarca());
+            stmt.setString(3, p.getModelo());
+            stmt.setString(4, p.getCodigo().toString());
+            stmt.setDouble(5, p.getValorCompra());
+            stmt.setDouble(6, p.getValorVenda());
+            stmt.setInt(7, p.getQtde());
+            if (p.getIdProduto() != null) {
+//                Update
+                stmt.setLong(8, p.getIdProduto());
+            }
+            int count = stmt.executeUpdate();
+            if (count == 0) {
+                throw new SQLException("Erro ao inserir o produto");
+            }
+            //Se inseriu, ler o id auto incremento
+            if (p.getIdProduto() == null) {
+                Long id = getGenerateId(stmt);
+                p.setIdProduto(id);
+            }
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+//    id gerado com campo auto increment
+
+    public static Long getGenerateId(Statement stmt) throws SQLException {
+        ResultSet rs = stmt.getGeneratedKeys();
+        if (rs.next()) {
+            Long id = rs.getLong(1);
+            return id;
+        }
+        return 0L;
+    }
+    public boolean delete(Long id) throws SQLException{
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement("delete from produto where idProduto=?");
+            stmt.setLong(1, id);
+            int count = stmt.executeUpdate();
+            boolean ok = count > 0;
+            return ok;
+        } finally {
+             if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
     }
     
 }//fech\ classe
---
+++ src/main/java/br/com/sudosu/sortemotores/services/ProdutoService.java
@@ -0,0 +1,67 @@
+package br.com.sudosu.sortemotores.services;
+
+import br.com.sudosu.sortemotores.dao.ProdutoDAO;
+import br.com.sudosu.sortemotores.domain.Produto;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Vinicius Vianna
+ */
+public class ProdutoService {
+
+    private ProdutoDAO db = new ProdutoDAO();
+//    Lista de todos os produtos do banco de dados
+
+    public List<Produto> getProdutos() {
+        try {
+            List<Produto> produtos = db.getProduto();
+            return produtos;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+//    Busca produto pelo id
+    public Produto getProduto(Long id){
+        try{
+            return db.getProdutoById(id);
+        }catch(SQLException e){
+           return null;
+        }
+    }
+//    Deleta o produto pelo id
+    public boolean delete(Long id){
+        try{
+            return db.delete(id);
+        }catch(SQLException e){
+            return false;
+        }
+    }
+//    Salva ou atualiza o produto
+      public boolean save(Produto produto){
+          try{
+              db.save(produto);
+              return true;
+          }catch(SQLException e){
+              return false;
+          }
+      }
+//      Busca o produto pelo nome
+      public List<Produto> findByName(String name){
+          try{
+             return db.findByName(name);
+          }catch(SQLException e){
+              return null;
+          }
+      }
+      public List<Produto> findByModelo(String modelo){
+          try {
+              return db.findByModelo(modelo);
+          } catch (SQLException e) {
+          return null;
+          }
+      }
+}//fecha classe
