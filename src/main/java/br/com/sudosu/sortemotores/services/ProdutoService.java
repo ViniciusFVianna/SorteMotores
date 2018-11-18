13b13204791af1bbe7a3ee0563231c13afc49191
Vinicius Vianna <vinicius-durap-16@hotmail.com>
2018-11-17 23:22:46 -0200

    Configuração de hibernate

--- src/main/java/br/com/sudosu/sortemotores/services/ProdutoService.java
+++ src/main/java/br/com/sudosu/sortemotores/services/ProdutoService.java
@@ -2,11 +2,9 @@ package br.com.sudosu.sortemotores.services;
 
 import br.com.sudosu.sortemotores.dao.ProdutoDAO;
 import br.com.sudosu.sortemotores.domain.Produto;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframwork.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -17,55 +15,37 @@ public class ProdutoService {
 
     @Autowired
     private ProdutoDAO db;
-//    Lista de todos os produtos do banco de dados
 
+//    Lista de todos os produtos do banco de dados
     public List<Produto> getProdutos() {
-        try {
-            List<Produto> produtos = db.getProduto();
-            return produtos;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return new ArrayList<>();
-        }
+        List<Produto> produtos = db.getProduto();
+        return produtos;
     }
+
 //    Busca produto pelo id
-    public Produto getProduto(Long id){
-        try{
-            return db.getProdutoById(id);
-        }catch(SQLException e){
-           return null;
-        }
+    public Produto getProduto(Long id) {
+        return db.getProdutoById(id);
     }
+    
 //    Deleta o produto pelo id
-    public boolean delete(Long id){
-        try{
-            return db.delete(id);
-        }catch(SQLException e){
-            return false;
-        }
+    @Transactional(rollbackFor = Exception.class)
+    public boolean delete(Long id) {
+        return db.delete(id);
     }
+    
 //    Salva ou atualiza o produto
-      public boolean save(Produto produto){
-          try{
-              db.save(produto);
-              return true;
-          }catch(SQLException e){
-              return false;
-          }
-      }
+    @Transactional(rollbackFor = Exception.class)
+    public boolean save(Produto produto) {
+        db.saveOrUpdate(produto);
+        return true;
+    }
+    
 //      Busca o produto pelo nome
-      public List<Produto> findByName(String name){
-          try{
-             return db.findByName(name);
-          }catch(SQLException e){
-              return null;
-          }
-      }
-      public List<Produto> findByModelo(String modelo){
-          try {
-              return db.findByModelo(modelo);
-          } catch (SQLException e) {
-          return null;
-          }
-      }
+    public List<Produto> findByName(String name) {
+        return db.findByName(name);
+    }
+
+    public List<Produto> findByModelo(String modelo) {
+        return db.findByModelo(modelo);
+    }
 }//fecha classe
--- src/test/java/test/SpringUtil.java
+++ src/test/java/test/SpringUtil.java
@@ -1,8 +1,14 @@
 package test;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.SessionFactoryUtils;
+import org.springframework.orm.hibernate5.SessionHolder;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 /**
  *
@@ -12,6 +18,9 @@ public class SpringUtil {
 
     private static SpringUtil instance;
     protected AbstractXmlApplicationContext ctx;
+    //Hibenate
+    protected HibernateTransactionManager txManager;
+    private Session session;
 
     private SpringUtil() {
         try {
@@ -33,6 +42,9 @@ public class SpringUtil {
         if(ctx == null){
             return null;
         }
+        if(session == null){
+            openSession();
+        }
         String[] beanNamesForType = ctx.getBeanNamesForType(c);
         if(beanNamesForType == null || beanNamesForType.length == 0){
             return null;
@@ -45,7 +57,41 @@ public class SpringUtil {
         if(ctx == null){
             return null;
         }
+        if(session == null){
+        openSession();
+    }
         Object bean = ctx.getBean(name);
         return bean;
+    }
+    /**
+     * Deixa a Session viva nesta thread. Mesma coisa que a thread de uma
+     * requisição web utilizandoo filtro "OpenSessionInViewFilter
+     */
+    public Session openSession(){
+        if(ctx != null){
+            txManager = (HibernateTransactionManager) ctx.getBean("transactionManager");
+            SessionFactory sessionFactory = txManager.getSessionFactory();
+            session = sessionFactory.openSession();
+            TransactionSynchronizationManager.bindResource(sessionFactory, new SessionHolder(session));
+        }
+        return session;
+    }
+    /**
+     * remove a Session da thread
+     */
+    public void closeSession(){
+        if(ctx != null && txManager != null){
+            SessionFactory sessionFactory = txManager.getSessionFactory();
+           TransactionSynchronizationManager.unbindResource(sessionFactory);
+           SessionFactoryUtils.closeSession(session);
+           session = null;
+        }
+    }
+    public Session getSession(){
+        return session;
+    }
+    public SessionFactory getSessionFactory(){
+        SessionFactory sf = txManager.getSessionFactory();
+        return sf;
     }
 }
